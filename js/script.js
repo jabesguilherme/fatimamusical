295a210840a5c7c4f6491d547be00487c697afd2
jabesguilherme <voltatoni.si@gmail.com>
2023-10-02 12:19:54 -0300

    Alterando VH para % e ajustando a página

--- js/script.js
+++ js/script.js
@@ -141,3 +141,18 @@ function stopRotation() {
 ;
 
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
